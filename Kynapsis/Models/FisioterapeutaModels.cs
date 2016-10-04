0de79f38c2024ee2913f32df6c6b2f479255119a
Diego Alejandro Ramírez <darasat@hotmail.com>
2016-10-04 16:53:49 -0500

    commit

---
+++ Kynapsis/Models/FisioterapeutaModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kynapsis.Models
+{
+    public class Fisioterapeuta
+    {
+        public string Documento { get; set; }
+        public string TipoDocumento { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public Nullable<System.DateTime> FechaNacimiento { get; set; }
+        public Nullable<int> Edad { get; set; }
+        public string Sexo { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+    }
+}
